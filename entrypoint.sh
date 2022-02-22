a99aeb9a924531e0e0232c8930a7c2c37e12d23a
Knight, Eddie <iv.eddieknight@gmail.com>
2022-02-22 15:26:39 -0600

    Added entrypoint troubleshooting logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,17 @@ echo "[default]" >> ~/.aws/credentials
 echo "aws_access_key_id=${AWS_ACCESS_KEY_ID}" >> ~/.aws/credentials
 echo "aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}" >> ~/.aws/credentials
 
+if [ -z "$AWS_ACCESS_KEY_ID" ]
+then
+      echo "\$AWS_ACCESS_KEY_ID is empty"
+else
+      echo "\$AWS_ACCESS_KEY_ID is NOT empty"
+fi
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]
+then
+      echo "\$AWS_SECRET_ACCESS_KEY is empty"
+else
+      echo "\$AWS_SECRET_ACCESS_KEY is NOT empty"
+fi
+
 /probr/cmd/probr
