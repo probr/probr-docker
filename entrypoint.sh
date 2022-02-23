76807471195b9f558907107fd3f27231e19142ac
Knight, Eddie <iv.eddieknight@gmail.com>
2022-02-23 10:05:01 -0600

    Added config file generation w/ env var AWS_REGION

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,11 @@ echo "[default]" >> ~/.aws/credentials
 echo "aws_access_key_id=${AWS_ACCESS_KEY_ID}" >> ~/.aws/credentials
 echo "aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}" >> ~/.aws/credentials
 
+echo "[default]" >> ~/.aws/config
+echo "region = ${AWS_REGION}" >> ~/.aws/config
+echo "output = json" >> ~/.aws/config
+
+
 if [ -z "$AWS_ACCESS_KEY_ID" ]
 then
       echo "\$AWS_ACCESS_KEY_ID is empty"
